3e9cc8f644c0c6c18c39b062e48ba314f633f5e0
mshegde <shegdemanoj@gmail.com>
2019-11-05 11:15:17 -0800

    Modified the chopstick picking randomization and catered to not more than 2 people eating simultaneously

--- DiningPhilosophers/diningPhilosopher.go
+++ DiningPhilosophers/diningPhilosopher.go
@@ -1,47 +1,86 @@
 package main
+
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 )
+
 var wg sync.WaitGroup
+var philLimit int
+var eating sync.WaitGroup
+var xMutex sync.RWMutex
 
 type CSticks struct {
 	sync.Mutex
 }
 
 type Phil struct {
-	leftCS *CSticks
+	leftCS  *CSticks
 	rightCS *CSticks
-	timesEaten int
-	philNum int
 }
 
-func (p Phil) eat(){
-	for p.timesEaten < 3 {
-		p.leftCS.Lock()
-		p.rightCS.Lock()
-		fmt.Printf("starting to eat %d\n", p.philNum)
-		p.timesEaten = p.timesEaten + 1
-		fmt.Printf("finishing eating %d\n", p.philNum)
-		p.rightCS.Unlock()
-		p.leftCS.Unlock()
+func (p Phil) eat(philosopher int, eatingRequest chan int) {
+
+	for i := 1; i <= 3; i++ {
+		chopPicked := rand.Intn(5)
+		eatingRequest <- 1
+		if chopPicked < 5 {
+			p.leftCS.Lock()
+			p.rightCS.Lock()
+		} else {
+			p.rightCS.Lock()
+			p.leftCS.Lock()
+		}
+		fmt.Printf("Starting to eat %d  \n", philosopher+1)
+		fmt.Printf("Finisihing to eat %d  \n", philosopher+1)
+
+		if chopPicked < 5 {
+			p.rightCS.Unlock()
+			p.leftCS.Unlock()
+		} else {
+			p.leftCS.Unlock()
+			p.rightCS.Unlock()
+		}
+		eating.Done()
+		xMutex.Lock()
+		philLimit -= 1
+		xMutex.Unlock()
 	}
 	wg.Done()
 }
 
-func main(){
+func hostRoutine(eatingRequest chan int) {
+
+	for {
+		<-eatingRequest
+		eating.Add(1)
+		xMutex.Lock()
+		philLimit += 1
+		if philLimit > 2 {
+			xMutex.Unlock()
+			eating.Wait()
+		} else {
+			xMutex.Unlock()
+		}
+	}
+}
+
+func main() {
+	eatingRequest := make(chan int, 1)
 	ChopSticks := make([]*CSticks, 5)
-	for i := 0; i < 5; i++{
+	for i := 0; i < 5; i++ {
 		ChopSticks[i] = new(CSticks)
 	}
 	philos := make([]*Phil, 5)
-	for i := 0; i < 5; i++{
-		philos[i] = &Phil{leftCS:ChopSticks[i], rightCS:ChopSticks[(i+1)%5], timesEaten:0, philNum:i+1}
+	for i := 0; i < 5; i++ {
+		philos[i] = &Phil{leftCS: ChopSticks[i], rightCS: ChopSticks[(i+1)%5]}
 	}
-	for i := 0; i < 5; i++{
-		wg.Add(1)
-		go philos[i].eat()
+	wg.Add(5)
+	go hostRoutine(eatingRequest)
+	for i := 0; i < 5; i++ {
+		go philos[i].eat(i, eatingRequest)
 	}
 	wg.Wait()
+	fmt.Println("All Philosophers have eaten!!!")
 }
-
